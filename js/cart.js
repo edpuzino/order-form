8036b496fc3b4983bfec19236b4362a42c47e52b
Wyatt Wedell <wyattearp24@aol.com>
2018-06-16 10:26:43 -0700

    started the css

--- js/cart.js
+++ js/cart.js
@@ -7,15 +7,15 @@ table.addEventListener('click', removeItemFromCart);
 var cart;
 
 function loadCart() {
-    var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-    cart = new Cart(cartItems);
+  var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+  cart = new Cart(cartItems);
 }
 
 // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
 function renderCart() {
-    loadCart();
-    clearCart();
-    showCart();
+  loadCart();
+  clearCart();
+  showCart();
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
@@ -24,20 +24,20 @@ function clearCart() {}
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
-    // TODO: Find the table body
+  // TODO: Find the table body
 
-    // TODO: Iterate over the items in the cart
-    // TODO: Create a TR
-    // TODO: Create a TD for the delete link, quantity,  and the item
-    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  // TODO: Iterate over the items in the cart
+  // TODO: Create a TR
+  // TODO: Create a TD for the delete link, quantity,  and the item
+  // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
 
 function removeItemFromCart(event) {
 
-    // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
-    // TODO: Save the cart back to local storage
-    // TODO: Re-draw the cart table
+  // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+  // TODO: Save the cart back to local storage
+  // TODO: Re-draw the cart table
 
 }
 
